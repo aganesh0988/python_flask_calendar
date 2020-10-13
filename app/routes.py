077722ea2e092a0fd3f2051886f967861f2aa573
Andrea <aganesh0988@gmail.com>
2020-10-13 16:11:17 -0500

    set up production environment

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,26 @@
 from flask import Blueprint, render_template
+import os
+import psycopg2
 
-bp = Blueprint('main', __name__,'' )
+bp = Blueprint('main', __name__, '')
+
+CONNECTION_PARAMETERS = {
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASS'),
+    'dbname': os.environ.get('DB_NAME'),
+    'host': os.environ.get('DB_HOST')
+}
+
 
 @bp.route('/')
 def main():
-	return render_template('main.html')
+    # create connection object as conn to connect to db
+    with psycopg2.connect(CONNECTION_PARAMETERS) as conn:
+        # create cursor object as curs to interact with db
+        with conn.cursor() as curs:
+            # execute sql command to alter db, retrieve data, etc.
+            # curs.execute(<sql command>)
+
+            # add data extracted from db to render_template arg list
+            # this displays db info in specified HTML template
+    return render_template('main.html')
