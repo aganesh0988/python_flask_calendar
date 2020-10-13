a16581215e51a195ac50a1bd28d911756f69ea6c
Andrea <aganesh0988@gmail.com>
2020-10-13 18:03:34 -0500

    handles posts

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 import os
 import psycopg2
 from .forms import AppointmentForm
+from datetime import datetime
+
 
 bp = Blueprint('main', __name__, '')
 
@@ -15,9 +17,32 @@ CONNECTION_PARAMETERS = {
 # add methods=['GET', 'POST'] so this route can accept form submission
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def main():
     form = AppointmentForm()
+    if form.validate_on_submit():
+        # do stuff - send data to db?
+        params = {
+            'name': form.name.data,
+            'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+            'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+            'description': form.description.data,
+            'private': form.private.data
+        }
+        # column_names = list(params.keys())
+        # column_values = list(params.values())
+        # create connection object as conn to connect to db
+        with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+            # create cursor object as curs to interact with db
+            with conn.cursor() as curs:
+                # execute sql command to alter db, retrieve data, etc.
+                # curs.execute("<sql insert statement>")
+                values = ', '.join(['%s'] * len(params))
+                columns = ', '.join(params.keys())
+                sql = "INSERT INTO appointments ( %s ) VALUES ( %s )" % (
+                    columns, values) '%s', '%s',
+                curs.execute(sql, list(params.values()))
+                redirect('/')
     # create connection object as conn to connect to db
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         # create cursor object as curs to interact with db
